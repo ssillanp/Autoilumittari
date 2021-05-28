c8058e487358d26302d59916dad1ce69e4b0f694
sami <sami.sillanpaa@iki.fi>
2021-05-28 18:48:49 +0300

    taas yksi versio

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from tripdata import car, trip
 
 app = Flask(__name__)
@@ -14,10 +14,18 @@ def index():
 
 @app.route("/results")
 def results():
-    trp1 = trip(request.args.get('dist'), request.args.get('speed1'), car(request.args.get('car')))
-    trp2 = trip(request.args.get('dist'), request.args.get('speed2'), car(request.args.get('car')))
+    try:
+        if request.args.get('car') is None:
+            raise ValueError
+        trp1 = trip(request.args.get('dist'), request.args.get('speed1'), car(request.args.get('car')))
+        trp2 = trip(request.args.get('dist'), request.args.get('speed2'), car(request.args.get('car')))
+    except ValueError:
+        return redirect('/error')
     return render_template('results.html', data=[trp1, trp2])
 
+@app.route("/error")
+def error():
+    return render_template('error.html')
 
 def main():
     app.run(debug=True)
