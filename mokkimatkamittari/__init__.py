40fa02e4d21f83ade9ffcc3948c0c1f5fa95812b
sami <sami.sillanpaa@iki.fi>
2021-05-31 20:46:20 +0300

    appkey changed

--- mokkimatkamittari/__init__.py
+++ mokkimatkamittari/__init__.py
@@ -4,6 +4,6 @@ import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY']=S3Connection(os.environ['ML_APPKEY'], os.environ['ML_APPKEY'])
+app.config['SECRET_KEY']="c25071b35496070b34a024ad6c0cf43ab0ee4174654ccb76845897afe5d8b396"
 
 from mokkimatkamittari import routes
