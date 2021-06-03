8cfef1204541cce662d2e78b4508c9cbdd134b4b
sami <sami.sillanpaa@iki.fi>
2021-06-03 19:51:21 +0300

    Added some docstrings

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Mökkimatkamittari
+# Autoilumittari
 
 ### Solidabis koodihaaste 2021: "autoilumittari"
 
@@ -9,9 +9,9 @@ webapp -> https://mokkimatkamittari.herokuapp.com
 - Flask + Flask_wtf (forms)
 - HTML + CSS
 
-### Asennus
+### Asennus ja käynnistäminen
 
-        > git clone https://github.com/ssillanp/Mokkimatkalaskuri.git
+        > git clone https://github.com/ssillanp/Autoilumittari.git
         > pip install -r requirements.txt
 
 testikäytössä yksinkertaisesti flask development serverillä 
@@ -21,6 +21,19 @@ testikäytössä yksinkertaisesti flask development serverillä
 tai tuotannossa 
 
         > gunicorn --workers=2 run:app
+
+### Kuvaus tehdystä ratkaisusta
+
+- Python toteutus, auto- ja laskenta toteutettu python luokkina.
+    - car-luokka: Malli- ja kulutustiedot
+    - trip-luo0kka: 
+- Web toteutus Python Flask- ja Flask_wtf-kirjastoilla.
+- Sivu HTML templatena ja ulkonäkö CSS
+- Tuotantoserverinä gunicorn.
+
+
+
+
 
 
 
--- autoilumittari/tripdata.py
+++ autoilumittari/tripdata.py
@@ -1,5 +1,6 @@
 
 class car:
+    """Class contains car info"""
     def __init__(self, model):
         self.model = model
         self.slope = 1.009
@@ -17,7 +18,7 @@ class car:
         return f"car({self.model})"
 
 class trip:
-
+    """Class contains trip info"""
     fuelprice = 1.60
 
     def __init__(self, dist, speed, car):
@@ -27,30 +28,58 @@ class trip:
 
     @property
     def trip_consumption(self):
-        speed_consumption = self.car.comsumption * (1.009 ** self.speed)
+        """
+        Calculate trip consumption, based on car, distance and speed
+        :return Trip consumption
+        """
+        speed_consumption = self.car.comsumption * (self.car.slope ** self.speed)
         return round(speed_consumption * self.dist / 100, 2)
 
     @property
     def trip_duration(self):
+        """
+        Calculate trip duration based on distance and speed
+        :return list(trip total time in hours, trip full hours, trip full minutes)
+        """
         trip_time = self.dist / self.speed
         return [trip_time, int(trip_time), int(trip_time%1*60)]
 
 
     def carbon_diff(self, other):
+        """
+        Calculate difference in co2 emissions
+        :param other: trip(speed2)
+        :return co2 difference in kg
+        """
         return abs(round((self.trip_consumption * 2.85)-(other.trip_consumption * 2.85), 2))
 
 
     def fuel_diff(self, other):
+        """
+           Calculate difference in fuel consumption
+           :param other: trip(speed2)
+           :return list(total difference, difference rounded)
+           """
         diff = abs(round(self.trip_consumption - other.trip_consumption, 2))
         return [diff, round(diff * self.fuelprice, 2)]
 
     def fuel_diff_prcent(self, other):
+        """
+        Calculate difference in fuel consumption in precentage
+        :param other: trip(speed2)
+        :return fuel consumption difference in precentage value
+        """
         if self.trip_consumption >= other.trip_consumption:
             return round((self.trip_consumption - other.trip_consumption) / self.trip_consumption * 100, 0)
         else:
             return round((other.trip_consumption - self.trip_consumption) / other.trip_consumption * 100, 0)
 
     def time_diff(self, other):
+        """
+        Calculate difference in trip time
+        :param other: trip(speed2)
+        :return list(trip full hours, trip full minutes)
+        """
         return [abs(int(self.trip_duration[0] - other.trip_duration[0])), int((self.trip_duration[0]- other.trip_duration[0])%1*60)]
 
     def __repr__(self):
