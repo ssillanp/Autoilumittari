dac77ea16e845ff1141a4ea10d58d3014d48dda1
sami <sami.sillanpaa@iki.fi>
2021-06-16 08:51:04 +0300

    README finished

--- autoilumittari/__init__.py
+++ autoilumittari/__init__.py
@@ -3,6 +3,6 @@ import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY']="c25071b35496070b34a024ad6c0cf43ab0ee4174654ccb76845897afe5d8b396"
+app.config['SECRET_KEY']="c25071b34496070b34a024ad6c0cf43ab0ee4176654ccb76845897afe5d8b396"
 
 from autoilumittari import routes
